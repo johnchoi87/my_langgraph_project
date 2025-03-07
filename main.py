c313d8efc190e047dc5722be605f96b3e259571f
최준우 <emotree@wavve.com>
2025-03-07 13:04:10 +0900

    프롬프트 수정 및 사용자 위치 정보 가져오는 구조 변경

--- main.py
+++ main.py
@@ -2,11 +2,69 @@ import streamlit as st
 from streamlit_js_eval import get_geolocation
 from my_langgraph_project.utils.graph import create_workflow
 from langchain_core.messages import ChatMessage
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+import googlemaps
+import os
+
+load_dotenv()
+# Google Maps API 키 설정
+key = os.getenv("GMAPS_API_KEY")
+gmaps = googlemaps.Client(key=key)
+
+
+def get_city_from_coordinates(lat: float, lng: float) -> str:
+    try:
+        # Reverse Geocoding 요청
+        reverse_geocode_result = gmaps.reverse_geocode((lat, lng), language='ko')
+        print(reverse_geocode_result)
+        # 도시 이름 추출
+        if reverse_geocode_result:
+            result_address = ""
+            locality = ""
+            administrative_area_level_1 = ""
+            sublocality_level_1 = ""
+            sublocality_level_2 = ""
+            for geocode_result in reverse_geocode_result:
+                for component in geocode_result['address_components']:
+                    if 'locality' in component['types']:
+                        locality = component['long_name']
+                    if 'administrative_area_level_1' in component['types']:
+                        administrative_area_level_1 = component['long_name']
+                    if 'sublocality_level_1' in component['types']:
+                        sublocality_level_1 = component['long_name']
+                    if 'sublocality_level_2' in component['types']:
+                        sublocality_level_2 = component['long_name']
+
+            if sublocality_level_1 is None or sublocality_level_1 == "":
+                sublocality_level_1 = locality
+            #result_address = f"도시 이름: {locality}, {administrative_area_level_1}, {sublocality_level_1}, {sublocality_level_2}"
+            result_address = f"{administrative_area_level_1} {sublocality_level_1} {sublocality_level_2}"
+            #print(result_address)
+            return result_address
+        else:
+            return "결과를 찾을 수 없습니다."
+    except Exception as e:
+        return f"오류 발생: {e}"
+
 
 st.set_page_config(page_title="나만의 알프레드", page_icon="🧑‍⚖️")
 st.title('나만의 알프레드 🧑‍⚖️')
-location_data = get_geolocation()
-print(location_data)
+
+
+if "my_location" not in st.session_state:
+    location_data = get_geolocation()
+    if location_data is not None and "coords" in location_data:
+        my_coords = location_data["coords"]
+        latitude = my_coords["latitude"]
+        longitude = my_coords["longitude"]
+        #latitude = 38.182081
+        #longitude = 128.549606
+        my_address = get_city_from_coordinates(latitude, longitude)
+        st.session_state["my_location"] = my_address
+        print(my_address)
+    else:
+        print("No location data")
 
 # 대화 내용을 저장할 세션 상태를 초기화합니다.
 if "messages" not in st.session_state:
@@ -34,13 +92,28 @@ if chat_input := st.chat_input('지역 정보나 날씨 관련된 질문을 남�
     add_history("user", chat_input)
     st.chat_message("user").write(chat_input)
 
-    final_message = ""  # 마지막 메시지를 저장할 변수
+    template = PromptTemplate(
+        input_variables=["user_input", "my_location"],
+        template= """
+        당신은 친절하고 유쾌한 AI입니다! 주어진 메시지 입력에 대한 🤖💡답변에 이모지를 적극 활용하고, 명확하고 간결한 방식으로 설명해주세요! 한글로 대답해주세요!🚀🔥
+        질문에 대상 장소에 대한 정보가 없다면 사용자의 현재 위치 정보를 참고하여 답변을 해주세요.
+        사용자가 특정 도시의 날씨를 요청하면, 해당 도시 이름을 영어로 변환하여 get_weather 도구를 호출하세요. "
+        예: '서울 날씨를 알려줘' -> get_weather(location='seoul')"
+        사용자가 특정 도시의 근처 장소를 요청하면, 해당 도시 이름을 영어로 변환하여 get_places 도구를 호출하세요. "
+        
+        질문: {user_input}
+        현재 위치: {my_location}
+        """
+    )
 
-    print(str(location_data["coords"]))
+    final_message = ""  # 마지막 메시지를 저장할 변수
+    prompt_text = template.format(user_input=chat_input, my_location=st.session_state["my_location"])
+    #print(str(location_data["coords"]))
     for chunk in st.session_state["graph"].stream(
             {
                 "messages": [
-                    ("human", f'{chat_input} coordinates: {str(location_data["coords"])}')
+                    ("human", prompt_text),
+                    #("human", f'{chat_input} coordinates: {str(location_data["coords"])}')
                 ],
             },
             stream_mode="values"
--- my_langgraph_project/utils/graph.py
+++ my_langgraph_project/utils/graph.py
@@ -20,38 +20,13 @@ gmaps = googlemaps.Client(key=key)
 
 
 # LangChain Tool 정의
-@tool("get_city_from_coordinates")
-def get_city_from_coordinates(coordinates: str) -> str:
-    """
-    주어진 위도와 경도 좌표 (latitude, longitude)로부터 도시 이름을 반환합니다.
-    입력 형식: "latitude,longitude" (예: "37.5665,126.9780")
-    """
-    try:
-        # 입력 좌표 파싱
-        lat, lng = map(float, coordinates.split(','))
-
-        # Reverse Geocoding 요청
-        reverse_geocode_result = gmaps.reverse_geocode((lat, lng), language='ko')
-        print(reverse_geocode_result)
-        # 도시 이름 추출
-        if reverse_geocode_result:
-            for component in reverse_geocode_result[0]['address_components']:
-                if 'locality' in component['types'] or 'sublocality_level_1' in component['types']:
-                    return f"도시 이름: {component['long_name']}"
-            return "도시 정보를 찾을 수 없습니다."
-        else:
-            return "결과를 찾을 수 없습니다."
-    except Exception as e:
-        return f"오류 발생: {e}"
-
-
 @tool
 def get_weather(location: str):
     """Call to get the current weather."""
     with st.spinner("날씨 정보를 가져오는 중입니다."):
         weather = OpenWeatherMapAPIWrapper()
         weather_data = weather.run(location)
-        print(weather_data)
+        # print(weather_data)
     return weather_data
 
 
@@ -62,14 +37,14 @@ def get_places(query: str):
     with st.spinner("장소 정보를 가져오는 중입니다."):
         places = GooglePlacesTool()
         places_data = places.run(query)
-        print(places_data)
+        # print(places_data)
     return places_data
 
 
-tools = [get_city_from_coordinates, get_weather, get_places]
+tools = [get_weather, get_places]
 
 tool_node = ToolNode(tools)
-model_with_tools = ChatOpenAI(model="gpt-4o-mini", temperature=0).bind_tools(tools)
+model_with_tools = ChatOpenAI(model="gpt-4o", temperature=0).bind_tools(tools)
 
 
 def should_continue(state: MessagesState):
@@ -84,15 +59,20 @@ def call_model(state: MessagesState):
     print(state)
     messages = state["messages"]
     # 프롬프트에 이모지를 추가하여 더 친근하고 생동감 있게 만듦 🎨✨
+    '''
     messages.append({
         "role": "system",
         "content": "당신은 친절하고 유쾌한 AI입니다! 🤖💡답변에 이모지를 적극 활용하고, 명확하고 간결한 방식으로 설명해주세요! 한글로 대답해주세요!🚀🔥"
                    "사용자가 특정 도시의 날씨를 요청하면, 해당 도시 이름을 영어로 변환하여 get_weather 도구를 호출하세요. "
                    "예: '서울 날씨를 알려줘' -> get_weather(location='seoul')"
+                   "사용자가 특정 도시의 근처 장소를 요청하면, 해당 도시 이름을 영어로 변환하여 get_places 도구를 호출하세요. "
+                   "입력 프롬프트에 도시명이 있으면 좌표 정보는 무시해줘"
                    "입력 프롬프트에 도시에 대한 정보가 없으면 get_city_from_coordinates 도구를 사용하여 도시 이름을 찾아주세요. "
     })
+    '''
     with st.spinner("AI가 답변을 준비하고 있습니다."):
         response = model_with_tools.invoke(messages)
+    # print(response)
     return {"messages": [response]}
 
 
@@ -110,14 +90,15 @@ def create_workflow():
     return app
 
 
-'''
-for chunk in app.stream(
+if __name__ == "__main__":
+    app = create_workflow()
+    app.get_graph().draw_mermaid_png(output_file_path="workflow.png")
+    '''
+    for chunk in app.stream(
         # {"messages": [("human", "분당 날씨는?")]}, stream_mode="values"
         # {"messages": [("human", "동대문구 장안1동 근처 아파트?")]},
         # {"messages": [("human", "오늘 서울에서 운동하기 날씨가 어때?")]},
         # {"messages": [("human", "여의도에서 따뜻한 국물 요리 맛집을 소개해줘")]},
-        {"messages": [("human", "현재 서울에 연날리기 하기에 어떄?")]},
-        stream_mode="values"
-):
-    chunk["messages"][-1].pretty_print()
-    '''
+        {"messages": [("human", "현재 서울에서 연날리기 하기에 어떄? coord:127.269311. 37.413294")]}, stream_mode="values"):
+        chunk["messages"][-1].pretty_print()
+'''
